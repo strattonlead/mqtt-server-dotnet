3cd0092a11d19d4e0101baba8297c438042ab340
Arndt Bieberstein <arndt.bieberstein@gmail.com>
2023-11-23 22:40:42 +0100

    ssl

--- MQTTServer/Services/CertificateProvider.cs
+++ MQTTServer/Services/CertificateProvider.cs
@@ -10,11 +10,34 @@ namespace MQTTServer.Services
         public X509Certificate2 GetCertificate()
         {
             var path = Environment.GetEnvironmentVariable("X509_CERTIFICATE_PATH");
-            if (!File.Exists(path))
+            if (!string.IsNullOrWhiteSpace(path))
             {
-                throw new FileNotFoundException($"No file at path: {path}. Please provide a certificate.");
+                if (!File.Exists(path))
+                {
+                    throw new FileNotFoundException($"No file at path: {path}. Please provide a certificate.");
+                }
+                return new X509Certificate2(File.ReadAllBytes(path));
             }
-            return new X509Certificate2(File.ReadAllBytes(path));
+
+            var certPemPath = Environment.GetEnvironmentVariable("CERT_PEM_PATH");
+            if (string.IsNullOrWhiteSpace(certPemPath))
+            {
+                throw new FileNotFoundException($"No file at path: {certPemPath}. Please provide a cert.pem.");
+            }
+
+            var keyPemPath = Environment.GetEnvironmentVariable("KEY_PEM_PATH");
+            if (string.IsNullOrWhiteSpace(keyPemPath))
+            {
+                throw new FileNotFoundException($"No file at path: {keyPemPath}. Please provide a key.pem.");
+            }
+
+            var certPem = File.ReadAllText(certPemPath);
+            var keyPem = File.ReadAllText(keyPemPath);
+
+            return X509Certificate2.CreateFromPem(
+                certPem,
+                keyPem
+            );
         }
     }
 }
